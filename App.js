a69997d8bf7531d4f4d3fb7f85ad3c60372e5801
Gustavo Viegas <gustavoviegas231914@gmail.com>
2021-03-21 22:40:56 -0300

    :construction: Ainda em trabalho

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 
+const Router = require('./Router.js')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/Vinhos', Router)
+
 app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:${3000}`)
+  console.log(`Servidor Rodando -> http://localhost:${3000}`)
 })
